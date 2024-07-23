ae687e67cdeaef109026d1021d791cb670bfbd91
Collins O. Odhiambo <comon928@gmail.com>
2024-07-23 12:02:20 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-[#cad2dc] dark:bg-[#0b172b]">{children}</body>
+      <body className="bg-[#cad2dc] dark:bg-[#0b172b]">{children}
+      <!--Start of Tawk.to Script-->
+        
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/669f711432dca6db2cb3dac2/1i3fd3etv';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+      </body>
     </html>
   );
 }
